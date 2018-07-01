777d137e0d008b427a2a235fd9e0b0ba805242fa
Tim Bachmann <tim.bachmann@stud.unibas.ch>
2018-07-01 23:07:13 +0200

    Add pdf printing

--- src/main/java/ch/scbirs/shop/orderexplorer/gui/report/ReportScreen.java
+++ src/main/java/ch/scbirs/shop/orderexplorer/gui/report/ReportScreen.java
@@ -1,20 +1,26 @@
 package ch.scbirs.shop.orderexplorer.gui.report;
 
 import ch.scbirs.shop.orderexplorer.model.Data;
+import ch.scbirs.shop.orderexplorer.util.LogUtil;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.print.*;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class ReportScreen extends BorderPane {
+    private static final Logger LOGGER = LogUtil.get();
 
+    //TODO: export as csv
 
     public ReportScreen(Data data) throws IOException {
 
@@ -41,19 +47,16 @@ public class ReportScreen extends BorderPane {
         stage.setScene(scene);
         stage.show();
     }
-
     /**
      * https://carlfx.wordpress.com/2013/07/15/introduction-by-example-javafx-8-printing/
      * https://stackoverflow.com/questions/31231021/javafx8-print-api-how-to-set-correctly-the-printable-area
      */
-    @FXML
-    private void print() {
+    private void print(Printer printer) {
         Node node = getCenter();
-        Printer printer = Printer.getDefaultPrinter();
         PageLayout pageLayout
                 = printer.createPageLayout(Paper.A4, PageOrientation.PORTRAIT, Printer.MarginType.HARDWARE_MINIMUM);
         PrinterAttributes attr = printer.getPrinterAttributes();
-        PrinterJob job = PrinterJob.createPrinterJob();
+        PrinterJob job = PrinterJob.createPrinterJob(printer);
         double scaleX
                 = pageLayout.getPrintableWidth() / node.getBoundsInParent().getWidth();
         double scaleY
@@ -69,5 +72,27 @@ public class ReportScreen extends BorderPane {
         }
         node.getTransforms().remove(scale);
     }
+
+    @FXML
+    private void print() {
+        Printer printer = Printer.getDefaultPrinter();
+        print(printer);
+    }
+
+    @FXML
+    private void printPdf() {
+        Optional<Printer> pdf = Printer.getAllPrinters().stream().filter(p -> p.getName().toLowerCase().contains("pdf")).findFirst();
+        if (pdf.isPresent()) {
+            LOGGER.info("Printer: " + pdf.get().getName());
+            print(pdf.get());
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("No pdf printer found.");
+            alert.show();
+        }
+    }
+
+
 
 }
