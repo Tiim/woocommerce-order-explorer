bad7085f58b038fe626b2ff9183505a07f12753e
Tim Bachmann <tim.bachmann@stud.unibas.ch>
2018-09-17 13:36:37 +0200

    Add search feature for products

--- src/main/java/ch/scbirs/shop/orderexplorer/gui/filter/SearchFilter.java
+++ src/main/java/ch/scbirs/shop/orderexplorer/gui/filter/SearchFilter.java
@@ -1,6 +1,8 @@
 package ch.scbirs.shop.orderexplorer.gui.filter;
 
 import ch.scbirs.shop.orderexplorer.model.remote.Order;
+import ch.scbirs.shop.orderexplorer.model.remote.Product;
+import ch.scbirs.shop.orderexplorer.util.Util;
 
 import java.util.function.Function;
 
@@ -15,12 +17,24 @@ public class SearchFilter implements Function<Order, Boolean> {
 
     @Override
     public Boolean apply(Order order) {
+        String data = gatherData(order);
+        return data.toLowerCase().contains(search);
+    }
+
+    public String gatherData(Order order) {
         StringBuilder b = new StringBuilder();
         b.append(order.getFirstName()).append(" ");
         b.append(order.getLastName()).append(" ");
         b.append(order.getEmail()).append(" ");
         b.append(order.getStatus()).append(" ");
         b.append(order.getNote()).append(" ");
-        return b.toString().toLowerCase().contains(search);
+
+        for (Product p : order.getProducts()) {
+            b.append(p.getName()).append(" ");
+            b.append(p.getSku()).append(" ");
+            b.append(Util.formatMap(p.getMeta())).append(" ");
+        }
+
+        return b.toString();
     }
 }
