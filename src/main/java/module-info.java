bd9c703ca0df61faaf4d28a81740c7217af0a0a5
Tiim <tim.bachmann96@gmail.com>
2021-09-27 14:43:05 +0200

    Add executable build

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,15 +2,17 @@
 module ch.scbirs.shop.orderexplorer {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.swing;
     requires com.fasterxml.jackson.databind;
     requires okhttp3;
     requires com.google.common;
     requires org.apache.commons.io;
     requires java.logging;
     requires org.apache.logging.log4j;
-    requires org.apache.poi;
+    //requires org.apache.poi;
     requires org.apache.pdfbox;
-    requires com.github.dhorions.boxable;
+    //requires com.github.dhorions.boxable;
 
     opens ch.scbirs.shop.orderexplorer.gui to javafx.fxml, javafx.graphics;
     opens ch.scbirs.shop.orderexplorer.gui.report to javafx.fxml, javafx.graphics;
